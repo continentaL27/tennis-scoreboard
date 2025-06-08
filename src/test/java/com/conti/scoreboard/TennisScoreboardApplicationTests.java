8e5af11be5893893123e539f3c9ebec19a4978bc
continentaL27 <qwertysixsev@gmail.com>
2025-06-08 19:38:50 +0300

    feat: init

---
+++ src/test/java/com/conti/scoreboard/TennisScoreboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conti.scoreboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennisScoreboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
